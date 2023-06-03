a100ea9e87ccfff6e43674d2e4db8d7e5e9736d0
eyupatahanli <eyupatahanli@gmail.com>
2023-06-03 17:36:07 +0300

    hata çözüldü script çalışır halde

--- tokenx_alarm.py
+++ tokenx_alarm.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 import time
 
 load_dotenv()
-#token_address = os.getenv("ben")
 
 #API anahtarı env dosyasından çekme
 api_key = os.getenv("api")
@@ -27,66 +26,52 @@ def send_message(kim=None, islem=None, token=None,value=None,islem_var=1):
         requests.get(url_telegram).json()
 
 while True:
-    #5 saniyede bir döngü çalışsın
-    #todo ilk çalışmada sorun olmaması için transactionu sona al
-
-    time.sleep(5)
-    islem_sayısı = len(transactions)
     # BscScan API'sine GET isteği göndererek işlem geçmişini alın
     url = f'https://api.bscscan.com/api?module=account&action=tokentx&address={token_address}&startblock=0&endblock=999999999&sort=asc&apikey={api_key}'
     response = requests.get(url)
     data = response.json()
-
-    # İşlem geçmişi verilerini işleyin status 0 sa veri çekme başarısız demektir
     if data['status'] == '1':
         transactions = data['result']
+        islem_sayısı = len(transactions)
 
-        if islem_sayısı < len(transactions):
-            #5 saniyede kaç islem yapıldı
-            fark = len(transactions)-islem_sayısı
-            print(f"eyup az önce {fark} işlem yaptı")
-            for i in range(islem_sayısı, len(transactions), 1):
-                if transactions[i]["from"] == token_address:
-                    parite = transactions[i]["tokenSymbol"]
-                    adet = transactions[i]["value"]
-                    #message = f"{i}.işlem türü satış, işlem paritesi{parite},adedi = {adet}"
-                    #todo:usdt ya da bnb değerini de ekle
-                    #url_telegram = url = f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chat_id}&text={message}"
-                    #requests.get(url_telegram).json()
-                    print(f"{i}.işlem türü satış, işlem paritesi{parite},adedi = {adet}")
-                    send_message("eyup","sattı",token=parite,value = adet)
-                if transactions[i]["to"] == token_address:
-                    parite = transactions[i]["tokenSymbol"]
-                    adet = transactions[i]["value"]
-                    #message = f"{i}.işlem türü satış, işlem paritesi{parite},adedi = {adet}"
-                    #todo:usdt ya da bnb değerini de ekle
-                    #url_telegram = url = f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chat_id}&text={message}"
-                    #requests.get(url_telegram).json()
-                    #todo:usdt ya da bnb değerini de ekle
-                    print(f"{i}.işlem türü alış, işlem paritesi{parite},adedi = {adet}")
-                    send_message("eyup", "aldı", token=parite, value=adet)
-                else:
-                    parite = transactions[i]["tokenSymbol"]
-                    print(f"başka bir işlem yapıldı.parite ={parite}")
-        else:
-            print("yeni işlem yok")
-            #message = "yeni işlem yok"
-            # todo:usdt ya da bnb değerini de ekle
-            #url_telegram = url = f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chat_id}&text={message}"
-            #requests.get(url_telegram).json()
-            #send_message("eyup", "", token=parite, value=adet)
+        # İşlem geçmişi verilerini işleyin status 0 sa veri çekme başarısız demektir
+        if data['status'] == '1':
+            transactions = data['result']
 
-            send_message(islem_var=0)
-
-
-
-
-
+            if islem_sayısı < len(transactions):
+                #5 saniyede kaç islem yapıldı
+                fark = len(transactions)-islem_sayısı
+                print(f"eyup az önce {fark} işlem yaptı")
+                for i in range(islem_sayısı, len(transactions), 1):
+                    if transactions[i]["from"] == token_address:
+                        parite = transactions[i]["tokenSymbol"]
+                        adet = transactions[i]["value"]
+                        #todo:usdt ya da bnb değerini de ekle
+                        print(f"{i}.işlem türü satış, işlem paritesi{parite},adedi = {adet}")
+                        send_message("eyup","sattı",token=parite,value = adet)
+                    if transactions[i]["to"] == token_address:
+                        parite = transactions[i]["tokenSymbol"]
+                        adet = transactions[i]["value"]
+                        #message = f"{i}.işlem türü satış, işlem paritesi{parite},adedi = {adet}"
+                        #todo:usdt ya da bnb değerini de ekle
+                        print(f"{i}.işlem türü alış, işlem paritesi{parite},adedi = {adet}")
+                        send_message("eyup", "aldı", token=parite, value=adet)
+                    else:
+                        parite = transactions[i]["tokenSymbol"]
+                        print(f"başka bir işlem yapıldı.parite ={parite}")
+            else:
+                print("yeni işlem yok")
+                send_message(islem_var=0)
+        time.sleep(5)
 
     else:
         print('API isteği başarısız oldu. Hata:', data['message'])
-
-#todo telegram ekle
+        telegram_token = os.getenv("telegram_token")
+        chat_id = os.getenv("chat_id")
+        message = 'API isteği başarısız oldu. Hata:', data['message']
+        url_telegram = f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chat_id}&text={message}"
+        requests.get(url_telegram).json()
+        time.sleep(5)
 
 
 """        # Her bir işlemi işleyin
